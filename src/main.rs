1946f27447735b7db9ee9ad10e7a6e4d6d856c9f
Borodin Gregory <grihabor@gmail.com>
2019-09-29 00:46:21 +0300

    Implement 3 column table

--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,27 @@ use std::ops::Add;
 struct Table0;
 struct Table1<A>(Column<A>);
 struct Table2<A, B>(Column<A>, Column<B>);
+struct Table3<A, B, C>(Column<A>, Column<B>, Column<C>);
 
 trait WithColumn<C, R> {
-    fn push(self, _: Column<C>) -> R;
+    fn column(self, _: Column<C>) -> R;
 }
 
 impl<A> WithColumn<A, Table1<A>> for Table0 {
-    fn push(self, col: Column<A>) -> Table1<A> {
+    fn column(self, col: Column<A>) -> Table1<A> {
         Table1(col)
     }
 }
 
 impl<A, B> WithColumn<B, Table2<A, B>> for Table1<A> {
-    fn push(self, col: Column<B>) -> Table2<A, B> {
+    fn column(self, col: Column<B>) -> Table2<A, B> {
         Table2(self.0, col)
+    }
+}
+
+impl<A, B, C> WithColumn<C, Table3<A, B, C>> for Table2<A, B> {
+    fn column(self, col: Column<C>) -> Table3<A, B, C> {
+        Table3(self.0, self.1, col)
     }
 }
 
@@ -37,22 +44,26 @@ impl<T> From<T> for Column<T> {
     }
 }
 
-impl<'a, T: Add<Output=T> + Copy> Add for &Column<Vec<T>> {
+impl<T: Add<Output = T> + Copy> Add for &Column<Vec<T>> {
     type Output = Column<Vec<T>>;
 
     fn add(self, rhs: Self) -> Self::Output {
         Column(
-            self.0.iter()
-            .zip(rhs.0.iter())
-            .map(|(a, b)| *a + *b)
-            .collect()
+            self.0
+                .iter()
+                .zip(rhs.0.iter())
+                .map(|(a, b)| *a + *b)
+                .collect(),
         )
     }
 }
 
 fn main() {
     let table = Table::new()
-        .push(vec![1, 2, 3].into())
-        .push(vec![2, 3, 4].into());
-    println!("{:?}", &table.0 + &table.1)
+        .column(vec![1, 2, 3].into())
+        .column(vec![2, 3, 4].into());
+    let sum = &table.0 + &table.1;
+    let table = table.column(sum);
+
+    println!("{:?}", table.2)
 }
